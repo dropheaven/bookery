ef11f2df3a28ceeb55e7698226358e6c2e85dc4c
Saad Ahmed <dropheaven@live.com>
2019-02-15 21:43:42 -0600

    defined a setter and getter for genre name and author's full name

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,22 @@
 class Book < ActiveRecord::Base
   belongs_to :author
   belongs_to :genre
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def author_full_name=(full_name)
+    author = Author.find_by(full_name: full_name)
+    self.author = author
+  end
+
+  def author_full_name
+    self.author ? self.author.full_name : nil 
+  end
 end
