5056d8f06c17840bfbdf3b8b291dc1d7756edcf2
Saad Ahmed <dropheaven@live.com>
2019-02-15 22:22:15 -0600

    basic validation for first and last name

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,9 @@ class Author < ActiveRecord::Base
   has_many :books
   has_many :genres, through: :books
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def full_name
     if middle_name != nil
       first_name + " " + middle_name + " " + last_name
