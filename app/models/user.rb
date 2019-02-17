8a12260f1f3b9be7e0b617b2235731bc5affaf3d
Saad Ahmed <dropheaven@live.com>
2019-02-17 16:49:31 -0600

    basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, presence: true, length: { in: 2..20 }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, 
+  format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
 end
