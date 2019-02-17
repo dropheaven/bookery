74eb9a8377e12d37af32f794aa904c1a31a5d02c
Saad Ahmed <dropheaven@live.com>
2019-02-17 16:51:05 -0600

    unique-index added to the email column of users to ensure username is unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_221018) do
+ActiveRecord::Schema.define(version: 2019_02_17_222926) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_02_17_221018) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
