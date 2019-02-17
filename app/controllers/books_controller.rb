fd49ea1f5b8f81fec6fdecf887d52237e7d9902d
Saad Ahmed <dropheaven@live.com>
2019-02-16 19:46:07 -0600

    books can now be deleted

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    # raise params.inspect
     @book = Book.new(book_params)
 
     if @book.valid?
@@ -27,19 +28,32 @@ class BooksController < ApplicationController
   end
 
   def update
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book.destroy
+    redirect_to books_path
   end
 
   private
-
     def book_params
       params.require(:book).permit(
         :title,
-        :release_date
+        :release_date,
+        :author_id,
+        :genre_id
       )
     end
+
+#     {"utf8"=>"✓",
+#  "authenticity_token"=>"UDAMZ7oGSytTMeKuVZXLCwEKCwoHWcl47DA/r68M5zjNSujE43Btdt0fA/pKLc72wZGU0MYQCoFLM82P+SwqXQ==",
+#  "book"=>{"title"=>"The Night", "author_id"=>"1", "genre_id"=>"1", "release_date"=>"1990-01-20"},
+#  "commit"=>"Create Book"}
 
     def set_book
       @book = Book.find(params[:id])
