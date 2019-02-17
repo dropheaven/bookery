74f0cdd5681ed3d1dd9104553d44801f97f46389
Saad Ahmed <dropheaven@live.com>
2019-02-17 17:30:44 -0600

    resources for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'books#index'
+
+  resources :users
+  resources :books, only: [:create, :update, :destroy]
+
   resources :authors, except: :show do
     resources :books
   end
-
-  resources :books, only: [:create, :update, :destroy]
 end
