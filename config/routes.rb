34a4d5cfbe812ef1e60f2f89e85ca968e079f620
Saad Ahmed <dropheaven@live.com>
2019-02-18 17:25:23 -0600

    routes for login/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'books#index'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
-  resources :books, only: [:create, :update, :destroy]
 
+  resources :books, only: [:create, :update, :destroy]
   resources :authors, except: :show do
     resources :books
   end
