3f2457a7297d0e88abbaa22a8433dec8541a7c05
Saad Ahmed <dropheaven@live.com>
2019-02-21 14:47:02 -0600

    omniauth route created

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/auth/github/callback', to: 'sessions#create'
+
   resources :users
 
   resources :books, only: [:new, :create, :update, :destroy]
