de8bf3dd004dd1c64f244edbc07a559fdd3853d9
Saad Ahmed <dropheaven@live.com>
2019-02-20 20:34:03 -0600

    flash message added when user logs out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     # raise params.inspect
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-      log_in user
+      log_in(user)
       redirect_to user_path(user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
+    flash[:danger] = "Logged out sucessfully"
     redirect_to root_url
   end
 
