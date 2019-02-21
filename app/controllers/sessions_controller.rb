43b144709741cae4c3a4a962a51e376a60521eb4
Saad Ahmed <dropheaven@live.com>
2019-02-21 15:27:16 -0600

    flash message for when user logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,13 @@ class SessionsController < ApplicationController
         u.password = SecureRandom.hex
       end
       log_in(user)
+      flash[:success] = "Logged in as #{user.username}"
       redirect_to user
     else
       user = User.find_by(username: params[:session][:username])
       if user && user.authenticate(params[:session][:password])
         log_in(user)
+        flash[:success] = "Logged in as #{user.username}"
         redirect_to user
       else
         flash.now[:danger] = 'Invalid email/password combination'
