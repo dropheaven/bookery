7d61ac6ebce237df370009ed67f54bd1e4ec947a
Saad Ahmed <dropheaven@live.com>
2019-02-21 16:01:15 -0600

    comments table added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_222926) do
+ActiveRecord::Schema.define(version: 2019_02_21_215124) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2019_02_17_222926) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["genre_id"], name: "index_books_on_genre_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "genres", force: :cascade do |t|
