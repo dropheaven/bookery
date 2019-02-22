5dd7972aea83edbcf42afff35682417f710f9ff8
Saad Ahmed <dropheaven@live.com>
2019-02-22 14:33:00 -0600

    user can post comments on a book's show page

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    # raise params.inspect
+    book = Book.find(params[:comment][:id])
+    comment = book.comments.build(content: params[:comment][:content], user: current_user)
+
+    if comment.save
+      flash[:success] = "Comment posted!"
+      redirect_to author_book_path(book.author , book)
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content, :id)
+    end
+end
