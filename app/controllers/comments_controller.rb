9499d1bca01d59dca4c4c2d00db8957dedb418d6
Saad Ahmed <dropheaven@live.com>
2019-02-22 18:06:33 -0600

    user must be logged in to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,17 @@
 class CommentsController < ApplicationController
   def create
     # raise params.inspect
-    book = Book.find(params[:comment][:id])
-    comment = book.comments.build(content: params[:comment][:content], user: current_user)
-
-    if comment.save
-      flash[:success] = "Comment posted!"
+      book = Book.find(params[:comment][:id])
+      comment = book.comments.build(content: params[:comment][:content], user: current_user)
+    if logged_in?
+      if comment.save
+        flash[:success] = "Comment posted!"
+        redirect_to author_book_path(book.author, book)
+      else
+        redirect_to author_book_path(book.author, book)
+      end
+    else
+      flash[:danger] = "Must be logged in to post comment"
       redirect_to author_book_path(book.author, book)
     end
   end
