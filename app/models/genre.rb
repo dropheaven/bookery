8a309f2ed135e99e3df6c53780c40546e610cb86
Saad Ahmed <dropheaven@live.com>
2019-03-03 21:18:55 -0500

    downcasing name before validations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,12 +2,11 @@ class Genre < ApplicationRecord
   has_many :books
   has_many :authors, -> { distinct }, through: :books
 
-  before_validation :make_title_case
+  before_validation :make_downcase
   validates :name, presence: true, uniqueness: true
 
   private
-    def make_title_case
-      # downcase first to avoid cases like "whaT IS this".titlecase => "Wha T Is This"
-	    self.name = self.name.downcase.titlecase
+    def make_downcase
+	    name.downcase!
     end
 end
