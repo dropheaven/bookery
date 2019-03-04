8f7b7b6af49ef57b5aaf0f06f842ad30934c4972
Saad Ahmed <dropheaven@live.com>
2019-03-03 21:11:07 -0500

    removed first, middle, and last name columns. Replaced these with a full_name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_215124) do
+ActiveRecord::Schema.define(version: 2019_03_04_011052) do
 
   create_table "authors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "middle_name"
-    t.string "last_name"
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
   end
 
   create_table "books", force: :cascade do |t|
