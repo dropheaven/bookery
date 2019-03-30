52aa69fe844fd459cecb0b7c2345d53f923a5b78
Saad Ahmed <dropheaven@live.com>
2019-03-30 19:06:52 -0400

    using book_id from fetch request to ensure comment is posted on the correct book show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :sign_in_to_access, only: [:create]
 
   def create
-    book = Book.find(params[:comment][:id])
+    book = Book.find(params[:comment][:book_id])
     comment = book.comments.build(content: params[:comment][:content], user: current_user)
     if comment.save
       flash[:success] = "Comment posted!"
