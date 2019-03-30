96663d0cc54a612921a277b925e6fb19bea67b1c
Saad Ahmed <dropheaven@live.com>
2019-03-30 12:22:42 -0400

    added a book class, created an updateBookDetails function to update the the returned data from the fetch request. Also, moved the comments part of the update to a separate function named commentsCreator

--- app/assets/javascripts/book_show.js
+++ app/assets/javascripts/book_show.js
@@ -1,22 +1,56 @@
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener('turbolinks:load', () => {  
   const button = document.querySelector('.btn.btn-success');
   if (!button) return; // not on book#show if this gets executed
-  
 
-  button.addEventListener('click', (event) => {
-    let addOne = parseInt(button.dataset.book) + 1
-    let url = `/authors/${button.dataset.author}/books/${addOne}.json`;
+  button.addEventListener('click', event => {
+    const addOne = parseInt(button.dataset.book) + 1
+    const url = `/authors/${button.dataset.author}/books/${addOne}.json`;
 
     fetch(url)
       .then(response => response.json())
       .then(book => {
-        document.querySelector('h2').textContent = book.title;
-        document.querySelector('#genre').textContent = book.genre.name;
-        document.querySelector('#release').textContent = book.release_year;
-        
+        updateBookDetails(book);
         button.dataset.book = addOne.toString();
       });
   });
 });
+
+class Book {
+  constructor(title, releaseYear, author, genre, comments) {
+    this.title = title;
+    this.releaseYear = releaseYear;
+    this.author = author;
+    this.genre = genre;
+    this.comments = comments;
+  }
+}
+
+const updateBookDetails = json => {
+  const book = new Book(json.title, json.release_year, json.author.full_name, json.genre.name, json.comments);
+  document.querySelector('h2').textContent = book.title;
+  document.querySelector('#release').textContent = book.releaseYear;
+  document.querySelector('h5').textContent = book.author;
+  document.querySelector('#genre').textContent = book.genre;
+  commentsCreator(book.comments);
+}
+
+const commentsCreator = commentsArray => {
+  const commentsList = document.querySelector('ul.list-unstyled');
+  commentsList.innerHTML = "";
+
+  commentsArray.forEach(comment => {
+    const commentBody = `
+      <li class="media">
+        <div class="media-body">
+        <span></span>
+        <span></span>
+        <blockquote>${comment.content}</blockquote>
+        </div>
+      </li>
+    `;
+
+    commentsList.innerHTML = commentBody; 
+  });
+}
 
 
