945bd0f808c68d02615e291603634e0ee91bde97
Saad Ahmed <dropheaven@live.com>
2019-03-30 14:46:20 -0400

    added the titleize prototype method to the book class

--- app/assets/javascripts/book_show.js
+++ app/assets/javascripts/book_show.js
@@ -23,13 +23,29 @@ class Book {
     this.genre = genre;
     this.comments = comments;
   }
+
+   titleize() {
+    const titleized = [];
+    this.author.split(' ').forEach((name, idx) => {
+      const splitFurther = name.split('');
+      if (splitFurther.some(char => char === '.' || char === '-')) {
+        let withIntials = "";
+        splitFurther.forEach(char => withIntials += char.toUpperCase());
+        titleized.push(withIntials);
+      } else {
+        titleized.push(name[0].toUpperCase() + name.slice(1).toLowerCase());
+      }
+    });
+
+    return titleized.join(' ');
+  }
 }
 
 const updateBookDetails = json => {
   const book = new Book(json.title, json.release_year, json.author.full_name, json.genre.name, json.comments);
   document.querySelector('h2').textContent = book.title;
   document.querySelector('#release').textContent = book.releaseYear;
-  document.querySelector('h5').textContent = book.author;
+  document.querySelector('h5').textContent = `by ${book.titleize()}`;
   document.querySelector('#genre').textContent = book.genre;
   commentsCreator(book.comments);
 }
