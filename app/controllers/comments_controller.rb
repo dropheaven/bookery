193d6ab353c0db63400928cb98353e23c180d702
Saad Ahmed <dropheaven@live.com>
2019-03-30 18:48:36 -0400

    rendering json version of show page if comment is saved successfully

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,11 @@ class CommentsController < ApplicationController
   before_action :sign_in_to_access, only: [:create]
 
   def create
-    # raise params.inspect
     book = Book.find(params[:comment][:id])
     comment = book.comments.build(content: params[:comment][:content], user: current_user)
     if comment.save
       flash[:success] = "Comment posted!"
-      redirect_to author_book_path(book.author, book)
+      render json: book, status: 201
     else
       flash[:notice] = "Could not post. Please make sure you're signed in and your post isn't empty."
       redirect_to author_book_path(book.author, book)
