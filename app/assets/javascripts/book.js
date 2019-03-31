c809b2f14217a557bb81cd8e727147080b6acc66
Saad Ahmed <dropheaven@live.com>
2019-03-31 18:57:04 -0400

    moved functions related to the book object from book_show to book.js

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -23,3 +23,43 @@ class Book {
     return titleized.join(' ');
   }
 }
+
+const bookShowEventListener = () => {
+  const mbButton = document.querySelector('.btn.btn-success');
+  if (!mbButton) return; // not on book#show if this gets executed
+
+  mbButton.addEventListener('click', event => {
+    const addOne = parseInt(mbButton.dataset.book) + 1
+    const url = `/authors/${mbButton.dataset.author}/books/${addOne}.json`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(book => {
+        updateBookDetails(book);
+        mbButton.dataset.book = addOne.toString();
+      });
+  });
+};
+
+/* helper functions */
+
+const updateBookDetails = json => {
+  const book = new Book(json.title, json.release_year, json.author.full_name, json.genre.name, json.comments);
+  document.querySelector('h2').textContent = book.title;
+  document.querySelector('#release').textContent = book.releaseYear;
+  document.querySelector('h5').textContent = `by ${book.titleize()}`;
+  document.querySelector('#genre').textContent = book.genre;
+  displayComments(book.comments);
+};
+
+const displayComments = commentsArray => {
+  const commentsList = document.querySelector('ul.list-unstyled');
+  commentsList.innerHTML = "";
+  let commentBody = "";
+
+  commentsArray.forEach(comment => {
+    commentBody += makeComment(comment);
+  });
+
+  commentsList.innerHTML = commentBody; 
+};
