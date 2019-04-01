7853bbd2660ac67fe89688d9e9985d7a1394c0b1
Saad Ahmed <dropheaven@live.com>
2019-04-01 06:20:17 -0400

    author_name attribute added

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,5 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :title, :release_year, :genre_name
+  attributes :title, :release_year, :author_name, :genre_name
   belongs_to :author, serializer: BookAuthorSerializer
   belongs_to :genre, serializer: BookGenreSerializer
   has_many :comments, serializer: BookCommentSerializer
