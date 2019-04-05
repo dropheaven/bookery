2ca0f2a2d75098111654e78c2cc601d3e4584481
Saad Ahmed <dropheaven@live.com>
2019-04-05 13:07:02 -0400

    removed flash messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,8 @@ class CommentsController < ApplicationController
     book = Book.find(params[:comment][:book_id])
     comment = book.comments.build(content: params[:comment][:content], user: current_user)
     if comment.save
-      flash[:success] = "Comment posted!"
       render json: book, status: 201
     else
-      flash[:notice] = "Could not post. Please make sure you're signed in and your post isn't empty."
       redirect_to author_book_path(book.author, book)
     end
   end
